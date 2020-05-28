28b49e151ef2cc3fe66855a373ae577d31591be8
gnostiphage <gnostiphage@gmail.com>
2020-05-27 22:02:46 -0500

    added wttr script and cron job

---
+++ update-git.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+############################
+# update-submodules.sh
+# This script updates all the submodules; I honestly don't know why it's necessary yet.
+############################
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}"   )" >/dev/null 2>&1 && pwd   )"
+
+git submodule update --remote --recursive
+
+# fzf has actual binaries that need installing
+~/.fzf/install
+
+# remove recursive links that fzf's install script creates (mistake? idk)
+rm -f $dir/tmux/tmux
+rm -f $dir/vim/vim
+rm -f $dir/oh-my-zsh/oh-my-zsh
+
+# if any of the parent directories' submodules get updated,
+# they'll need their plugin links again
+ln -s ../../../powerlevel10k $dir/oh-my-zsh/custom/themes/powerlevel10k
+ln -s ../../../zsh-syntax-highlighting $dir/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+ln -s ../tmux-plugins $dir/tmux/plugins
---
+++ wttr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+############################
+# wttr.sh
+# This script pulls weather data for use in the local tmux,
+# getting called by a cron job so as to not call it every 
+# time the status bar updates
+############################
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}"   )" >/dev/null 2>&1 && pwd   )"
+
+if ps -ef | grep -v grep | grep tmux ; then
+    curl -s wttr.in?format=%c🌡️%t%20%w%20%m\&?m > $dir/wttr.txt
+    exit 0
+else
+    exit 0
+fi
